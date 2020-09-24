7c734ac22c93bcfcbb51512778da6106aad8a9c1
praveen-barawal <praveen.in.7@gmail.com>
2020-09-24 10:14:20 +0530

    3 files modified

--- src/app/toggle-implementation/switch.component.ts
+++ src/app/toggle-implementation/switch.component.ts
@@ -1,40 +1,41 @@
 import { Component, Input } from '@angular/core';
-import { SlideInOutAnimation } from '../Animation/animation';
+import { trigger, transition, animate, style } from '@angular/animations'
 
 @Component({
   selector: 'switch',
   templateUrl: './switch.component.html',
-   animations: [SlideInOutAnimation]
+    
 })
 export class SwitchComponent  {
   @Input() isChecked: boolean;
   @Input() className: string;
-
+  
   showProgress:boolean = false;
   showText:string = 'Devices Off';
   canShowWarn:boolean = true;
   canShowPrimary:boolean = false;
   colorClass:string = 'warn';
   spinnerClass :string = "mat-off-progress";
-  animationState:string = 'in';
+  isShowLoader:boolean = false;
 
   showTextAndLoader(showText: string){
     setTimeout(()=>{        
        this.showText = showText;
        this.showProgress = false;
+       this.isShowLoader = false;
     }, 2000);
   }
 
   onClick(){
      this.showProgress = true;
-      this.colorClass ="progress";
+     this.colorClass ="progress";
+     this.isShowLoader = true;
      this.showText = 'Please Wait...';
      if(this.isChecked){
        this.animationState = 'out';
        this.colorClass = 'primary';
        this.spinnerClass = "mat-on-progress";
        this.showTextAndLoader('Devices On');
-          
      }
      else{
       this.animationState = 'in';
