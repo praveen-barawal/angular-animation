b32808f7466380274270f623d7075dab571d6a01
praveen-barawal <praveen.in.7@gmail.com>
2020-09-24 14:26:55 +0530

    4 files modified

--- src/app/Animation/animation.ts
+++ src/app/Animation/animation.ts
@@ -10,87 +10,35 @@ import {
   keyframes
 } from "@angular/animations";
 
+
+
 export const AngularAnimation = [
-  trigger("slideInOut", [
-    state(
-      "in",
-      style({
-        "max-height": "500px",
-        opacity: "1",
-        visibility: "visible"
-      })
-    ),
-    state(
-      "out",
-      style({
-        "max-height": "0px",
-        opacity: "0",
-        visibility: "hidden"
-      })
-    ),
-    transition("in => out", [
-      group([
-        animate(
-          "40ms ease-in-out",
-          style({
-            opacity: "0"
-          })
-        ),
-        animate(
-          "600ms ease-in-out",
-          style({
-            "max-height": "0px"
-          })
-        ),
-        animate(
-          "700ms ease-in-out",
-          style({
-            visibility: "hidden"
-          })
-        )
-      ])
-    ]),
-    transition("out => in", [
-      group([
-        animate(
-          "1ms ease-in-out",
-          style({
-            visibility: "visible"
-          })
-        ),
-        animate(
-          "600ms ease-in-out",
-          style({
-            "max-height": "500px"
-          })
-        ),
-        animate(
-          "800ms ease-in-out",
-          style({
-            opacity: "1"
-          })
-        )
-      ])
-    ])
-  ]),
   trigger("slideInOutUp", [
     transition(":leave", [
       style({ zIndex: "1" }),
-      animate("400ms ease-in", style({ transform: "translateY(-100%)" })),
+      animate("400ms ease-in", style({ transform: "translateY(-50%)" })),
       transition(":enter", [
-        style({ transform: "translateY(-100%)", zIndex: "1" }),
+        style({ transform: "translateY(-20%)", zIndex: "1" }),
         animate(
           "400ms ease-in",
-          style({ transform: "translateY(0%)", zIndex: "1" })
+          style({ transform: "translateY(-100%)", zIndex: "1" })
         )
       ])
     ])
   ]),
-  trigger("shrinkOut", [
-    state("in", style({ height: "*" })),
-    transition("* => void", [
-      style({ height: "*" }),
-      animate(250, style({ height: 0 }))
+
+  trigger("slideInOutDown", [
+    transition(":leave", [
+      style({ zIndex: "1" }),
+      animate("400ms ease-in", style({ transform: "translateY(50%)" })),
+      transition(":enter", [
+        style({ transform: "translateY(20%)", zIndex: "1" }),
+        animate(
+          "400ms ease-in",
+          style({ transform: "translateY(100%)", zIndex: "1" })
+        )
+      ])
     ])
-  ])
+  ]),
+  
 ];
--- src/app/toggle-implementation/switch.component.ts
+++ src/app/toggle-implementation/switch.component.ts
@@ -1,50 +1,60 @@
-import { Component, Input } from '@angular/core';
-import { trigger, transition, animate, style } from '@angular/animations'
-import { AngularAnimation } from '../Animation/animation';
+import { Component, Input } from "@angular/core";
+import { trigger, transition, animate, style } from "@angular/animations";
+import { AngularAnimation } from "../Animation/animation";
+import {
+  slideInLeftAnimation,
+  slideInRightAnimation,
+  slideOutLeftAnimation,
+  slideOutRightAnimation
+} from "angular-animations";
 
 @Component({
-  selector: 'switch',
-  templateUrl: './switch.component.html',
-   animations: [AngularAnimation],
-    
+  selector: "switch",
+  templateUrl: "./switch.component.html",
+  animations: [
+    AngularAnimation,
+    slideInLeftAnimation(),
+    slideInRightAnimation(),
+    slideOutLeftAnimation(),
+    slideOutRightAnimation()
+  ]
 })
-export class SwitchComponent  {
+export class SwitchComponent {
   @Input() isChecked: boolean;
   @Input() className: string;
-  
-  showProgress:boolean = false;
-  showText:string = 'Devices Off';
-  canShowWarn:boolean = true;
-  canShowPrimary:boolean = false;
-  colorClass:string = 'warn';
-  spinnerClass :string = "mat-off-progress";
-  animationState:string = 'in';
-  isShowLoader:boolean = false;
 
-  showTextAndLoader(showText: string){
-    setTimeout(()=>{        
-       this.showText = showText;
-       this.showProgress = false;
-       this.isShowLoader = false;
+  showProgress: boolean = false;
+  showText: string = "Devices Off";
+  canShowWarn: boolean = true;
+  canShowPrimary: boolean = false;
+  colorClass: string = "warn";
+  spinnerClass: string = "mat-off-progress";
+  animationState: string = "in";
+  isShowLoader: boolean = false;
+
+  showTextAndLoader(showText: string) {
+    setTimeout(() => {
+      this.showText = showText;
+      this.showProgress = false;
+      this.isShowLoader = false;
     }, 2000);
   }
 
-  onClick(){
-     this.showProgress = true;
-     this.colorClass ="progress";
-     this.isShowLoader = true;
-     this.showText = 'Please Wait...';
-     if(this.isChecked){
-       this.animationState = 'out';
-       this.colorClass = 'primary';
-       this.spinnerClass = "mat-on-progress";
-       this.showTextAndLoader('Devices On');
-     }
-     else{
-      this.animationState = 'in';
-       this.colorClass = 'warn';
-       this.spinnerClass = "mat-off-progress";
-       this.showTextAndLoader('Devices Off');
-     }
+  onClick() {
+    this.showProgress = true;
+    this.colorClass = "progress";
+    this.isShowLoader = true;
+    this.showText = "Please Wait...";
+    if (this.isChecked) {
+      this.animationState = "out";
+      this.colorClass = "primary";
+      this.spinnerClass = "mat-on-progress";
+      this.showTextAndLoader("Devices On");
+    } else {
+      this.animationState = "in";
+      this.colorClass = "warn";
+      this.spinnerClass = "mat-off-progress";
+      this.showTextAndLoader("Devices Off");
+    }
   }
 }
